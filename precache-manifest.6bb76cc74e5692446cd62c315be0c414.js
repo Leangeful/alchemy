e750c7f691d90eb726a19d46ed769d6065d65493
Leange <cuttheleash1@gmail.com>
2020-02-23 13:26:18 +0100

    Updates

---
+++ precache-manifest.6bb76cc74e5692446cd62c315be0c414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dce5d3bd7aef14704940f235e8657c6",
+    "url": "/alchemy/index.html"
+  },
+  {
+    "revision": "255f30fda6776b603942",
+    "url": "/alchemy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "316039a3e75a4ee0a9e4",
+    "url": "/alchemy/static/js/2.f966957c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/alchemy/static/js/2.f966957c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255f30fda6776b603942",
+    "url": "/alchemy/static/js/main.bf07fb28.chunk.js"
+  },
+  {
+    "revision": "425d061c9051b4cfb59d",
+    "url": "/alchemy/static/js/runtime-main.bba90985.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alchemy/static/media/logo.5d5d9eef.svg"
+  }
+]);
